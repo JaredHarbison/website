3e69929808674ebe4127ad96996a4f0c350d7e25
Jared Harbison <jared.harbison@gmail.com>
2023-12-01 01:13:33 -0500

    feature new: implement better logo, WIP for mobile stack of SVG words and increase sizes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.scss';
 import './fontawesome.js';
 
 import Background from './Components/Background/Background.jsx';
+import LogoBanner from './Components/LogoBanner/LogoBanner.jsx';
 
 function App() {
   return (
     <div className="App">
       <Background/>
+      <LogoBanner/>
     </div>
   );
 }
