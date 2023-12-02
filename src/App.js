3c6051eb1665e833c76dbe65ce674e01b862985a
Jared Harbison <jared.harbison@gmail.com>
2023-12-01 21:52:42 -0500

    feature new: implement techstack section with fewer svgs from v1, implement accessibility text for svgs

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './fontawesome.js';
 
 import Background from './Components/Background/Background.jsx';
 import LogoBanner from './Components/LogoBanner/LogoBanner.jsx';
+import TechStack from './Components/TechStack/TechStack.jsx';
 
 function App() {
   return (
     <div className="App">
       <Background/>
       <LogoBanner/>
+      <TechStack/>
     </div>
   );
 }
