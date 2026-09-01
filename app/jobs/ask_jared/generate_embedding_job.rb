372427df9025b84eb87c502b72cf6362fe36df1a
Jared Harbison <jared.harbison@gmail.com>
2026-09-01 08:22:20 -0400

    fix(ask-jared): use ActiveRecord knowledge model in job

--- app/jobs/ask_jared/generate_embedding_job.rb
+++ app/jobs/ask_jared/generate_embedding_job.rb
@@ -3,7 +3,7 @@ module AskJared
     queue_as :default
 
     def perform(knowledge_entry_id)
-      entry = KnowledgeEntry.find(knowledge_entry_id)
+      entry = ::KnowledgeEntry.find(knowledge_entry_id)
       EmbeddingService.new.generate!(entry)
     end
   end
